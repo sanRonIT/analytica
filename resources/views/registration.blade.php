d2d57efdb4ca3087916f6f0a0e19cba1e0868223
BALA SAI <balasai1999@gmail.com>
2020-10-03 11:41:14 +0530

    Registration page

---
+++ resources/views/registration.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<title>Registration page</title>
+	<!--Bootstrap-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<!--CSS-->
+	<link href="/css/app.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+</head>
+
+<body class="bg-theme">
+
+	<!-- Start wrapper-->
+	<div id="wrapper" class="mt-3">
+		<div class="card card-authentication1">
+			<div class="card-body pr-4">
+				<div class="card-content">
+					<div class="text-center logo">
+						<img src="/images/bg-images/Logo.png" alt="logo icon">
+					</div>
+
+					<div class=letterspacing>
+						<form style="margin-top:55px;">
+							<div class="form-group">
+								<div class="position-relative has-icon-left name">
+									Enter your Name
+									<div class="gap">
+										<input type="text" id="exampleInputName" class="form-control input-shadow">
+
+									</div>
+									<div class="form-control-position">
+										<img src="/images/bg-images/icon1.svg">
+									</div>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<div class="position-relative has-icon-left name">
+									Enter your Email ID
+									<div class="gap">
+										<input type="text" id="exampleInputEmailId" class="form-control input-shadow">
+									</div>
+									<div class="form-control-position">
+										<img src="/images/bg-images/icon2.svg">
+									</div>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<div class="position-relative has-icon-left name">
+									Choose Password
+									<div class="gap">
+										<input type="text" id="exampleInputPassword" class="form-control input-shadow">
+									</div>
+									<div class="form-control-position">
+										<img src="/images/bg-images/icon3.svg">
+									</div>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<div class="position-relative has-icon-left name">
+									Re Enter Password
+									<div class="gaps">
+										<input type="text" id="exampleInputPassword" class="form-control input-shadow">
+									</div>
+									<div class="form-control-position">
+										<img src="/images/bg-images/icon3.svg">
+									</div>
+								</div>
+							</div>
+					</div>
+
+					<div class="form-group">
+						<div class="icheck-material-white">
+							<input type="checkbox" id="user-checkbox" checked="" />
+							<label for="user-checkbox" class="terms">I heraby, agree all the terms and condition</label>
+						</div>
+					</div>
+					<button style="text-align:center;" type="button" class="btn btn-light btn-block waves-effect waves-light signup center">Sign Up</button>
+				</div>
+
+
+				<svg class="svg1" height="52" width="42">
+					<line x1="0" y1="0" x2="130" y2="70" style="stroke:rgb(236, 235, 235);stroke-width:1" />
+				</svg>
+				<svg class="svg2" height="37" width="80">
+					<line x1="0" y1="0" x2="130" y2="70" style="stroke:rgb(236, 235, 235);stroke-width:1" />
+				</svg>
+
+				<span class="dot">
+
+				</span>
+
+				</form>
+			</div>
+		</div>
+	</div>
+	</div>
+	<!--wrapper-->
+	<!-- Bootstrap core JavaScript-->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+
+</html>
